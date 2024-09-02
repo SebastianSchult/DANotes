a0f1fba9b4b0186194f126ab238fbbdec6d4f527
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-09-02 07:55:53 +0200

    datas collectionData from firebase

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
+import { Firestore,collectionData, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  items$;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.items$ = collectionData(this.getNotesRef());
+  }
+
+
+  
+  //const itemCollection = collection(this.firestore, 'items');
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingeDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
