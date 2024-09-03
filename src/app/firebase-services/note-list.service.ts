524bc49ec4252e3562cdfed5cde07d4ac03c31bd
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-09-03 09:03:01 +0200

    firebase online link

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,22 +1,74 @@
 import { Injectable } from '@angular/core';
 import { inject } from '@angular/core';
-import { Firestore,collectionData, collection, doc } from '@angular/fire/firestore';
+import { Note } from '../interfaces/note.interface';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  doc,
+  onSnapshot,
+} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class NoteListService {
 
-  items$;
+
+@Injectable({
+  providedIn: 'root',
+})
+
+
+
+export class NoteListService {
+  
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  unsubNotes;
+  unsubTrash;
+
   firestore: Firestore = inject(Firestore);
 
   constructor() {
-    this.items$ = collectionData(this.getNotesRef());
+    this.unsubNotes = this.subNotesList();
+    this.unsubTrash = this.subTrashList();
   }
 
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach((element) => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
 
-  
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach((element) => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+
+  }
+
+  setNoteObject(obj: any, id: string): Note {
+    return{
+      id: id,
+      type: obj.type || "note",
+      title: obj.title || "", 
+      content: obj.content || "",
+      marked: obj.marked || false,
+    }
+  }
+
+  ngonDestroy() {
+    this.unsubTrash();
+    this.unsubNotes();
+  }
+
+  //this.items$ = collectionData(this.getNotesRef());
+
   //const itemCollection = collection(this.firestore, 'items');
 
   getNotesRef() {
--- src/app/note-list/note-list.component.ts
+++ src/app/note-list/note-list.component.ts
@@ -19,8 +19,12 @@ export class NoteListComponent {
   favFilter: "all" | "fav" = "all";
   status: "notes" | "trash" = "notes";
 
-  constructor(public noteService: NoteListService) {
+  constructor(private noteService: NoteListService) {
     this.noteList = this.getDummyData()
+  }
+
+  getList(){
+    return this.noteService.normalNotes;
   }
 
   changeFavFilter(filter:"all" | "fav"){
